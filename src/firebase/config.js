5b27ed57c8142a7e79967f81dab0025f13d2bea3
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-04-22 20:54:00 -0300

    component Register

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firebase'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEo8jdkVhoWAvHTMfBPoLkwvVk7IBdwX0",
+  authDomain: "miniblog-fb49f.firebaseapp.com",
+  projectId: "miniblog-fb49f",
+  storageBucket: "miniblog-fb49f.appspot.com",
+  messagingSenderId: "127549153613",
+  appId: "1:127549153613:web:4f22adf9392c2a8c209704"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
