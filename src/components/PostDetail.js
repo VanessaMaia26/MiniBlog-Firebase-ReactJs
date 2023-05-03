ed2aac1a932bf87792933ffc0bc85a310249877f
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-05-03 13:36:08 -0300

    new commit

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 import styles from "./PostDetail.module.css";
 
 const PostDetail = ({ post }) => {
-
-    console.log(post)
   return (
     <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
       <p className={styles.createdby}>por: {post.createdBy}</p>
       <div className={styles.tags}>
-        {post.tagsArray.map((tag) => (
+        {post.tags.map((tag) => (
           <p key={tag}>
             <span>#</span>
             {tag}
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBEo8jdkVhoWAvHTMfBPoLkwvVk7IBdwX0",
-  authDomain: "miniblog-fb49f.firebaseapp.com",
-  projectId: "miniblog-fb49f",
-  storageBucket: "miniblog-fb49f.appspot.com",
-  messagingSenderId: "127549153613",
-  appId: "1:127549153613:web:4f22adf9392c2a8c209704"
+  apiKey: "AIzaSyCLvEGhcQo1QjbHP-0D7bRaKrFAaGQW_sg",
+  authDomain: "miniblog-ref.firebaseapp.com",
+  projectId: "miniblog-ref",
+  storageBucket: "miniblog-ref.appspot.com",
+  messagingSenderId: "411532945241",
+  appId: "1:411532945241:web:3808bd6c6bf5b1fb774e48",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
